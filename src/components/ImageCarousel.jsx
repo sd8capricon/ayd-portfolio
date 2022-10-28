f5cb53941576f9547535d97501c9546fce203d4b
sd8capricon <72347505+sd8capricon@users.noreply.github.com>
2022-10-28 19:33:58 +0530

    home linked with cms

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,60 +1,95 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import React, {useEffect, useState} from 'react';
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import {Carousel} from 'react-bootstrap';
 
-// images
-import image1 from "../images/carousel/1.jpg";
-import image2 from "../images/carousel/2.jpg";
-import image3 from "../images/carousel/3.jpg";
-import image4 from "../images/carousel/4.jpg";
-import image5 from "../images/carousel/5.jpg";
-import image6 from "../images/carousel/6.jpg";
 
-export default function ImageCarousel(){
-    return(
+export default function ImageCarousel() {
+
+    //*[_type=='home']|order(sr_no){_id,name,image}
+
+    const client = sanityClient({
+        projectId: process.env.REACT_APP_PROJECT_ID,
+        dataset: 'production',
+        apiVersion: '2021-10-21',
+        useCdn: true
+    })
+    const builder = imageUrlBuilder(client)
+    const urlBuilder = (source) => {
+        return builder.image(source)
+    }
+
+    const [imgs, setImgs] = useState([])
+
+    useEffect(() => {
+        let query = "*[_type=='home']|order(sr_no){_id,name,image}"
+        let imgArr = []
+        client.fetch(query).then(res => {
+            res.forEach(ele => {
+                imgArr.push(urlBuilder(ele.image.asset).url())
+            })
+            setImgs(imgArr)
+        })
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+    const buildSlide = (img, key)=>{
+        return (
+            <Carousel.Item interval={5000} key={key}>
+                <img
+                    src={img}
+                    alt={"carousel slide "+key}
+                    className="carousel-img"
+                />
+            </Carousel.Item>
+        )
+    }
+
+    return (
         <div className="carousel">
             <Carousel indicators={false} pause={false}>
-                <Carousel.Item interval={7000}>
+                {imgs.map(buildSlide)}
+                {/*<Carousel.Item interval={7000}>
                     <img
                         className="carousel-img"
                         src={image1}
                         alt="First Slide"
                     />
                 </Carousel.Item>
-                <Carousel.Item interval={7000}>
+                    <Carousel.Item interval={7000}>
                     <img
-                        className="carousel-img"
-                        src={image2}
-                        alt="Second Slide"
+                    className="carousel-img"
+                    src={image2}
+                    alt="Second Slide"
                     />
-                </Carousel.Item>
-                <Carousel.Item interval={7000}>
+                    </Carousel.Item>
+                    <Carousel.Item interval={7000}>
                     <img
-                        className="carousel-img"
-                        src={image3}
-                        alt="Third Slide"
+                    className="carousel-img"
+                    src={image3}
+                    alt="Third Slide"
                     />
-                </Carousel.Item>
-                <Carousel.Item interval={7000}>
+                    </Carousel.Item>
+                    <Carousel.Item interval={7000}>
                     <img
-                        className="carousel-img"
-                        src={image4}
-                        alt="Fourth Slide"
+                    className="carousel-img"
+                    src={image4}
+                    alt="Fourth Slide"
                     />
-                </Carousel.Item>
-                <Carousel.Item interval={7000}>
+                    </Carousel.Item>
+                    <Carousel.Item interval={7000}>
                     <img
-                        className="carousel-img"
-                        src={image5}
-                        alt="Fifth Slide"
+                    className="carousel-img"
+                    src={image5}
+                    alt="Fifth Slide"
                     />
-                </Carousel.Item>
-                <Carousel.Item interval={7000}>
+                    </Carousel.Item>
+                    <Carousel.Item interval={7000}>
                     <img
-                        className="carousel-img"
-                        src={image6}
-                        alt="Sixth Slide"
+                    className="carousel-img"
+                    src={image6}
+                    alt="Sixth Slide"
                     />
-                </Carousel.Item>
+                    </Carousel.Item>*/}
             </Carousel>
         </div>
     );
